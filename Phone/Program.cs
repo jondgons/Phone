bafc1870be91156223e3d4b2868d2152913229e8
Jonathan Gonsowski <jondgons@ut.utm.edu>
2019-09-12 15:42:22 -0500

    Add project files.

---
+++ Phone/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class PhoneTypeChecker
+    {
+
+    }
+
+    enum Manufacturers
+    {
+
+    }
+
+    interface IPhoneFactory
+    {
+
+    }
+
+    interface ISmart
+    {
+
+    }
+
+    interface IDumb
+    {
+
+    }
+}
